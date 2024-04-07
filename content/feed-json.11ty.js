f0310438209ebce41c6b36c0ab7e1a50d155aaa8
thoughtsunificator <oss@thoughtsunificator.me>
2024-04-07 21:16:05 +0200

    Feed xml

---
+++ content/feed-json.11ty.js
@@ -0,0 +1,30 @@
+exports.data = {
+	eleventyExcludeFromCollections: true,
+	permalink: "/feed.json",
+	robots: {
+		ignore: true
+	}
+}
+
+exports.render = function(data) {
+	return JSON.stringify({
+		"version": "https://jsonfeed.org/version/1",
+		"title": data.site.name,
+		"home_page_url": data.site.url,
+		"feed_url": data.site.url + "/feed.json",
+		"description": data.site.description,
+		"author": {
+			"name": data.site.author.name,
+			"url": data.site.url
+		},
+		"items": data.feed.items.filter(rssItem => !rssItem.tags.find(tag => data.tagIndexBlacklist.includes(tag))).map(rssItem => {
+			const item = {
+				"id": rssItem.id,
+				"url": `${data.site.url}/${rssItem.getSlug.call(this)}.html`,
+				"title": rssItem.title,
+				"updated": new Date(rssItem.pubDate * 1000).toISOString()
+			}
+			return item
+		})
+	})
+}
---
+++ content/feed-xml.11ty.js
@@ -0,0 +1,29 @@
+exports.data = {
+	eleventyExcludeFromCollections: true,
+	permalink: "/feed.xml",
+	robots: {
+		ignore: true
+	}
+}
+
+exports.render = function(data) {
+	return `<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+	<title>${ data.site.name }</title>
+	<subtitle>${ data.site.description }</subtitle>
+	<link href="${data.site.url}/feed.xml" rel="self"/>
+	<link href="${data.site.url}"/>
+	<id>${data.site.feed.id}</id>
+	<author>
+		<name>${data.site.author.name}</name>
+	</author>
+	${data.feed.items.filter(rssItem => !rssItem.tags.find(tag => data.tagIndexBlacklist.includes(tag))).map(rssItem => {
+		return `<entry>
+			<id>${rssItem.id}</id>
+			<link href="${data.site.url}/${rssItem.getSlug.call(this)}.html"/>
+			<title>${ rssItem.title }</title>
+			<updated>${new Date(rssItem.pubDate * 1000).toISOString()}</updated>
+		</entry>`
+	}).join("")}
+</feed>`
+}
