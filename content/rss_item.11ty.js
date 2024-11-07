54a99a8cce163eafbbf784d812441ac1deda7215
thoughtsunificator <oss@thoughtsunificator.me>
2024-11-07 14:35:31 +0100

    h1 textContent

--- content/rss_item.11ty.js
+++ content/rss_item.11ty.js
@@ -33,8 +33,11 @@ exports.render = function(data) {
 	for(const nodesToRemove of nodesToRemoves.slice()) {
 		nodesToRemove.remove()
 	}
+	const h1 = document.createElement("h1")
+	h1.style = "flex-grow: 1"
+	h1.textContent = data.rssItem.title
 	return `
-		<h1 style="flex-grow: 1;">${data.rssItem.title}</h1>
+		${h1.outerHTML}
 		<div class="date"><b>${new Date(data.rssItem.pubDate * 1000).toISOString()}</b></div>
 		<small>${data.rssItem.url ? `<a href="/urls/${data.rssItem.url.getSlug.call(this)}">${data.rssItem.url.title}</a>` : `${feedURL.hostname + feedURL.pathname}`}</small>
 		<div>${data.rssItem.tags.map(tag => ` <a${tag == "source" ? ' style="font-weight: bold"' : ""} href="/tags/${this.slugify(tag)}.html">${tag}</a>`).join(" | ") }</div>
